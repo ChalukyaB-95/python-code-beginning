9a7593848cf79eb7370ce4dc366d69b4603f68e3
Chalukya <chalukya95@gmail.com>
2026-06-11 07:49:00 +0530

    version-2

--- p2.py
+++ p2.py
@@ -3,3 +3,22 @@ if str(a)==str(a)[::-1]:
      print(a,"is a palindrome")
 else:
      print(a,"is not a palindrome")
+
+positive = []
+negative = []
+zero = []
+
+a1 = list(range(-25,26))
+
+for i in a1:
+    if i > 0:
+        positive.append(i)
+    elif i < 0:
+        negative.append(i)
+    else:
+        zero.append(i)
+
+print("Positive:", positive)
+print("Negative:", negative)
+print("Zero:", zero)
+
